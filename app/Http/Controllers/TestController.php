e809dba4a73ce7ab624297efa30ad884e28a5f01
vasymus <vasymuszchd@mail.ru>
2020-07-31 18:39:42 +0300

    ~ TransferServicesAndArticles.php: store images add html new and replaced with new images src

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,15 +6,14 @@ use App\Services\RandomProxies\Contracts\CanGetRandomProxies;
 use App\Services\RandomProxies\Repositories\RandomProxiesCacheRepository;
 use App\Services\TransferFromOrigin\TransferGallery;
 use App\Services\TransferFromOrigin\TransferServicesAndArticles;
+use Illuminate\Support\Facades\Storage;
 
 class TestController extends Controller
 {
     public function test()
     {
         $tr = new TransferServicesAndArticles();
-        $path = $tr->fetchAndStoreHtml("/ukladka-shtuchnogo-parketa/");
-
-        dump($path);
+        //$tr->transfer();
 
         return view('test');
     }
--- app/Services/TransferFromOrigin/TransferServicesAndArticles.php
+++ app/Services/TransferFromOrigin/TransferServicesAndArticles.php
@@ -3,6 +3,7 @@
 namespace App\Services\TransferFromOrigin;
 
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\DomCrawler\Crawler;
 
 class TransferServicesAndArticles extends BaseTransfer
 {
@@ -17,7 +18,6 @@ class TransferServicesAndArticles extends BaseTransfer
         parent::__construct("https://parket-lux.ru", null, null, $fetcher);
     }
 
-
     public function transfer()
     {
         $raw = require(storage_path("app/seeds/pages/raw.php"));
@@ -25,30 +25,110 @@ class TransferServicesAndArticles extends BaseTransfer
         $seeds = [];
 
         foreach ($raw as $item) {
-
+            $seed = $this->handleRawSeedItem($item);
+            if (!empty($seed)) $seeds[] = $seed;
         }
 
         Storage::put("seeds/pages/seeds.json", json_encode($seeds, JSON_UNESCAPED_UNICODE));
     }
 
-    public function fetchAndStoreHtml(string $url): ?string
+    public function handleRawSeedItem(array $item): ?array
     {
+        $url = $item["link"];
+        $images = $item["images"];
         $html = $this->fetchHtml($url);
         if (!$html) {
             dump("Failed to fetch html $url");
             return null;
         }
-        if ($this->isArticle($url)) {
-            $path = "seeds/pages/articles/" . basename($url) . ".html";
-            $isSaved = Storage::put($path, $html);
+        $this->handleStoreHtml($url, $html);
 
+        $seo = $this->parseSeo($html);
+        $oldNewImages = [];
+        foreach ($images as $oldImageSrc) {
+            $newImageSrc = $this->fetchAndStoreImage($oldImageSrc);
+            if ($newImageSrc) $oldNewImages[$oldImageSrc] = $newImageSrc;
+        }
+        $updatedHtml = $this->getHtmlWithUpdatedImgSrc($html, $oldNewImages);
+
+        if ($this->isArticle($url)) {
+            $path = "seeds/pages/new/articles/" . basename($url) . ".html";
+            $isSaved = Storage::put($path, $updatedHtml);
         } else {
-            $path = "seeds/pages/services/" . basename($url) . ".html";
+            $path = "seeds/pages/new/services/" . basename($url) . ".html";
+            $isSaved = Storage::put($path, $updatedHtml);
+        }
+
+        if (!$isSaved) {
+            dump("Failed to save new html $url");
+            return null;
+        }
+
+        return [
+            "name" => $seo["h1"] ?? "",
+            "slug" => basename($url),
+            "html" => $path,
+            "seo" => $seo,
+            "is_active" => true,
+            "description" => null,
+            "is_article" => $this->isArticle($url),
+            "images" => array_values($oldNewImages),
+            "_oldUrl" => $url,
+        ];
+    }
+
+    public function parseSeo(string $html): array
+    {
+        $crawler = new Crawler($html);
+        $result = [
+            "title" => null,
+            "description" => null,
+            "keywords" => null,
+            "h1" => null,
+        ];
+        $crawler->filter("meta")->each(function(Crawler $node) use(&$result) {
+            if ($node->attr("name") === "keywords") {
+                $result["keywords"] = $node->attr("content");
+            }
+            if ($node->attr("name") === "description") {
+                $result["description"] = $node->attr("content");
+            }
+        });
+        $crawler->filter("title")->each(function(Crawler $node) use(&$result) {
+            $result["title"] = $node->text();
+        });
+        $crawler->filter("h1")->each(function(Crawler $node) use(&$result) {
+            $result["h1"] = $node->text();
+        });
+
+        return $result;
+    }
+
+    public function getHtmlWithUpdatedImgSrc(string $html, array $oldNewImages): string
+    {
+        $crawler = new Crawler($html);
+
+        foreach ($oldNewImages as $old => $new) {
+            $crawler->filter("img")->each(function(Crawler $node) use($old, $new) {
+                if ($node->attr("src") === $old) $node->getNode(0)->setAttribute("src", $new);
+            });
+        }
+
+        return $crawler->html();
+    }
+
+    public function handleStoreHtml(string $url, string $html): ?string
+    {
+        if ($this->isArticle($url)) {
+            $path = "seeds/pages/old/articles/" . basename($url) . ".html";
+            $isSaved = Storage::put($path, $html);
+        } else {
+            $path = "seeds/pages/old/services/" . basename($url) . ".html";
             $isSaved = Storage::put($path, $html);
         }
 
         if (!$isSaved) {
-            dump("Failed to save html $url");
+            dump("Failed to save old html $url");
             return null;
         }
         return $path;
