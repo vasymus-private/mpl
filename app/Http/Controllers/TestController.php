bd7933f84864fdd61e5c62ae289f951e02a83f55
vasymus <vasymuszchd@mail.ru>
2021-01-08 19:23:30 +0200

    start implementing trimming

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\FAQ;
 use App\Models\Product\Product;
 use App\Services\TransferFromOrigin\TransferFAQ2;
+use App\Services\TruncateHTML\TruncateHTML;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
@@ -14,7 +16,12 @@ class TestController extends Controller
 {
     public function test()
     {
-//        (new TransferFAQ2())->parsePages();
+        /** @var FAQ $faq */
+        $faq = FAQ::query()->first();
+
+        return TruncateHTML::truncateWords($faq->question, 20);
+
+        dump();
 
         return view('test');
     }
--- resources/views/web/layouts/sidebar-menu.blade.php
+++ resources/views/web/layouts/sidebar-menu.blade.php
@@ -7,20 +7,20 @@
 <div class="sidebar-left js-sticky-sidebar">
     <div class="inner-wrapper-sticky js-inner-wrapper-sticky">
         <div class="sidebar-left__inner">
-            <x-sidebar-menu-services />
+            <x-sidebar-menu-services/>
 
-            <x-sidebar-menu-materials />
+            <x-sidebar-menu-materials/>
 
             <!--<div class="sidebar-payment">-->
             <!--<div class="sidebar-payment__title">Любые способы оплаты</div>-->
             <!--<div class="row-line row-line__between">-->
-            <!--<div class="sidebar-payment__column"><img src="{{asset("images//wallet.png")}}" alt=""></div>-->
-            <!--<div class="sidebar-payment__column"><img src="{{asset("images//credit-cards.png")}}" alt=""></div>-->
-            <!--<div class="sidebar-payment__column"><img src="{{asset("images//bank.png")}}" alt=""></div>-->
+        <!--<div class="sidebar-payment__column"><img src="{{asset("images//wallet.png")}}" alt=""></div>-->
+        <!--<div class="sidebar-payment__column"><img src="{{asset("images//credit-cards.png")}}" alt=""></div>-->
+        <!--<div class="sidebar-payment__column"><img src="{{asset("images//bank.png")}}" alt=""></div>-->
             <!--</div>-->
             <!--</div>-->
 
-            <x-sidebar-menu-cart />
+            <x-sidebar-menu-cart/>
 
             <div class="sidebar-left__fav">
                 <a href="{{route('aside')}}" class="sidebar-left__fav-link">Отложенные товары</a>
@@ -28,7 +28,7 @@
             </div>
 
 
-            <x-sidebar-menu-viewed />
+            <x-sidebar-menu-viewed/>
 
             <ul class="sidebar-list-block">
                 <li class="sidebar-list-block__item">
@@ -44,35 +44,37 @@
                     </p>
                 </li>
             </ul>
+
             <div class="sidebar-faq-block">
                 <h4 class="sidebar-faq-block__title">Вопросы и ответы:</h4>
                 <div class="sidebar-faq-block__row">
-                	<div class="sidebar-faq-block__content">
-                		 <p>Хочу сделать пол на лагах в новостройке. Фото и план прилагаю.</p>
-                		 <p>Прошу смету:</p>
-                		 <p><u>На первый этап</u></p>
-                		 <p>...</p>
+                    <div class="sidebar-faq-block__content">
+                        <p>Хочу сделать пол на лагах в новостройке. Фото и план прилагаю.</p>
+                        <p>Прошу смету:</p>
+                        <p><u>На первый этап</u></p>
+                        <p>...</p>
                     </div>
-                	<div class="sidebar-faq-block__more">
-                		<a href="#">Читать полностью</a>
-                	</div>
+                    <div class="sidebar-faq-block__more">
+                        <a href="#">Читать полностью</a>
+                    </div>
                 </div>
                 <div class="sidebar-faq-block__row">
-                	<div class="sidebar-faq-block__content">
-                	    <p>Добрый день!</p>
-                	    <p>Я живу в старом доме. Деревянные полы не ровные. Перепад от стен до центра комнаты до 4 см. Хочу...</p>
-                 	</div>
-                	<div class="sidebar-faq-block__more">
-                		<a href="#">Читать полностью</a>
-                	</div>
+                    <div class="sidebar-faq-block__content">
+                        <p>Добрый день!</p>
+                        <p>Я живу в старом доме. Деревянные полы не ровные. Перепад от стен до центра комнаты до 4 см.
+                            Хочу...</p>
+                    </div>
+                    <div class="sidebar-faq-block__more">
+                        <a href="#">Читать полностью</a>
+                    </div>
                 </div>
                 <div class="sidebar-faq-block__row">
-                	<div class="sidebar-faq-block__content">
-                		 <p>Здравствуйте! Скрипит паркет. Срочно надо устранить скрип. Циклевать, и т.д. срочно.</p>
-                	</div>
-                	<div class="sidebar-faq-block__more">
-                		<a href="#">Читать полностью</a>
-                	</div>
+                    <div class="sidebar-faq-block__content">
+                        <p>Здравствуйте! Скрипит паркет. Срочно надо устранить скрип. Циклевать, и т.д. срочно.</p>
+                    </div>
+                    <div class="sidebar-faq-block__more">
+                        <a href="#">Читать полностью</a>
+                    </div>
                 </div>
                 <div class="sidebar-faq-block__apply-question">
                     <a href="/ask/">Задать свой вопрос</a>
