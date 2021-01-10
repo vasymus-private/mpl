25a9cb11e19c7128bd96e2151c5953938c69efc3
sergey <sergey.martishko@yandex.ru>
2021-01-10 21:06:49 +0200

    add some comments

--- resources/views/web/pages/gallery-items/gallery-item.blade.php
+++ resources/views/web/pages/gallery-items/gallery-item.blade.php
@@ -2,20 +2,22 @@
 
 @section('page-content')
     <x-h1 :entity="$subtitle"></x-h1>
-    <ul>
-        @foreach($childGalleryItems as $index => $galleryItem)
-            <?php /** @var \App\Models\GalleryItem $galleryItem */ ?>
-            <li>
-                <div>
-                    <a data-fancybox="gallery-1" href="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->description}}" data-caption="{{$galleryItem->description}}">Увеличить</a>
-                </div>
-                <div>
-                    <a data-fancybox="gallery-2" href="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->description}}" data-caption="{{$galleryItem->description}}">
-                        <img style="max-width: 200px;" src="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->description}}" />
-                    </a>
-                </div>
-                <div><p>{!! $galleryItem->description  !!}</p></div>
-            </li>
-        @endforeach
-    </ul>
+    <div class="photo-gallery-wrapper">
+        <ul>
+            @foreach($childGalleryItems as $index => $galleryItem)
+                <?php /** @var \App\Models\GalleryItem $galleryItem */ ?>
+                <li>
+                    <div>
+                        <a data-fancybox="gallery-1" href="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->description}}" data-caption="{{$galleryItem->description}}">Увеличить</a>
+                    </div>
+                    <div>
+                        <a data-fancybox="gallery-2" href="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->description}}" data-caption="{{$galleryItem->description}}">
+                            <img style="max-width: 200px;" src="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->description}}" />
+                        </a>
+                    </div>
+                    <div><p>{!! $galleryItem->description  !!}</p></div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
--- resources/views/web/pages/gallery-items/gallery-items.blade.php
+++ resources/views/web/pages/gallery-items/gallery-items.blade.php
@@ -1,16 +1,18 @@
 @extends('web.pages.page-layout')
 
 @section('page-content')
-    <x-h1 :entity="$subtitle"></x-h1>
-    <ul>
-        @foreach($galleryItems as $index => $galleryItem)
-            <?php /** @var \App\Models\GalleryItem $galleryItem */ ?>
-            <li>
-                <div><b>{{$index + 1}}</b></div>
-                <div><a href="{{route("gallery.items.show", $galleryItem->slug)}}">{{$galleryItem->name}}</a></div>
-                <div><img src="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->name}}" title="{{$galleryItem->description}}" width="150" height="110" /></div>
-                <div><a href="{{route("gallery.items.show", $galleryItem->slug)}}">Открыть фотоальбом</a></div>
-            </li>
-        @endforeach
-    </ul>
+    <div class="photo-gallery-wrapper">
+        <h1 class="photo-gallery-wrapper__title">Фотогалерея паркетных работ</h1>
+        <ul class="photo-gallery-wrapper__list">
+            @foreach($galleryItems as $index => $galleryItem)
+                <?php /** @var \App\Models\GalleryItem $galleryItem */ ?>
+                <li class="photo-gallery-wrapper__item">
+                    <div><b>{{$index + 1}}</b></div>
+                    <div><a href="{{route("gallery.items.show", $galleryItem->slug)}}">{{$galleryItem->name}}</a></div>
+                    <div><img src="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->name}}" title="{{$galleryItem->description}}" width="150" height="110" /></div>
+                    <div><a href="{{route("gallery.items.show", $galleryItem->slug)}}">Открыть фотоальбом</a></div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
