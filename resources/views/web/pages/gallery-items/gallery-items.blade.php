616b5f5620d308a6d105b16414acd9c8a7891f68
sergey <sergey.martishko@yandex.ru>
2021-01-10 21:36:00 +0200

    add some comments

--- resources/views/web/pages/gallery-items/gallery-items.blade.php
+++ resources/views/web/pages/gallery-items/gallery-items.blade.php
@@ -7,10 +7,12 @@
             @foreach($galleryItems as $index => $galleryItem)
                 <?php /** @var \App\Models\GalleryItem $galleryItem */ ?>
                 <li class="photo-gallery-wrapper__item">
-                    <div><b>{{$index + 1}}</b></div>
-                    <div><a href="{{route("gallery.items.show", $galleryItem->slug)}}">{{$galleryItem->name}}</a></div>
-                    <div><img src="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->name}}" title="{{$galleryItem->description}}" width="150" height="110" /></div>
-                    <div><a href="{{route("gallery.items.show", $galleryItem->slug)}}">Открыть фотоальбом</a></div>
+                    <div class="photo-gallery-wrapper__row">
+                        <div class="photo-gallery-wrapper__count"><b>{{$index + 1}}</b></div>
+                        <div class="photo-gallery-wrapper__subtitle"><a href="{{route("gallery.items.show", $galleryItem->slug)}}">{{$galleryItem->name}}</a></div>
+                    </div>
+                    <div class="photo-gallery-wrapper__img-block"><img src="{{$galleryItem->getFirstMediaUrl(\App\Models\GalleryItem::MC_MAIN_IMAGE)}}" alt="{{$galleryItem->name}}" title="{{$galleryItem->description}}" width="150" height="110" /></div>
+                    <div class="photo-gallery-wrapper__view-all"><a href="{{route("gallery.items.show", $galleryItem->slug)}}">Открыть фотоальбом</a></div>
                 </li>
             @endforeach
         </ul>
