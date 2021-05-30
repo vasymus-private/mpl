224d30ffd7bbd9d6972203315dc5eb70cfbdab60
sergey <sergey.martishko@yandex.ru>
2021-05-30 17:48:02 +0300

    add markup for product

--- resources/views/admin/livewire/includes/form-group-input.blade.php
+++ resources/views/admin/livewire/includes/form-group-input.blade.php
@@ -1,7 +1,7 @@
 @if($isRow ?? true)
     <div class="form-group row">
-        <label for="{{$field}}" class="col-sm-3 col-form-label {{$labelClass ?? ''}}">{{$label}}:</label>
-        <div class="col-sm-9">
+        <label for="{{$field}}" class="col-sm-5 col-form-label {{$labelClass ?? ''}}">{{$label}}:</label>
+        <div class="col-sm-7 {{$className ?? ''}}">
             @include('admin.livewire.includes.form-control-input', ['field' => $field])
         </div>
     </div>
