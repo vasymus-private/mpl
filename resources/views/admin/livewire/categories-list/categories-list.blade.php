503c353f2c5cf0c585b77b91012293408dc54139
vasily <developer@344764-cy56414.tmweb.ru>
2021-06-06 19:41:25 +0000

    hosting commit

---
+++ resources/views/admin/livewire/categories-list/categories-list.blade.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * @var array[] $categories @see {@link \Domain\Products\DTOs\ProductItemAdminDTO[]}
+ * @var string|int|null $category_id
+ * @var string|int|null $category_name
+ * @var bool $editMode
+ * @var \App\Http\Livewire\Admin\CategoriesList $this
+ */
+?>
+<div>
+    @include('admin.livewire.includes.form-search', [
+        'submit' => 'handleSearch',
+        'newRoute' => route('admin.categories.create'),
+        'clearAll' => 'clearAllFilters',
+        'prepends' => $this->getPrepends(),
+    ])
+
+    <div class="table-responsive position-relative">
+        <div wire:loading.flex>
+            <div class="d-flex justify-content-center align-items-center bg-light" style="opacity: 0.5; position:absolute; top:0; bottom:0; right:0; left:0; z-index: 20; ">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        </div>
+
+        <table class="table">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">
+                    <div class="form-check">
+                        <input wire:model="selectAll" @if($editMode) disabled @endif class="form-check-input position-static" type="checkbox">
+                    </div>
+                </th>
+                <th scope="col"><span class="main-grid-head-title">&nbsp;</span></th>
+                <th scope="col"><span class="main-grid-head-title">ID</span></th>
+                <th scope="col"><span class="main-grid-head-title">Название</span></th>
+                <th scope="col"><span class="main-grid-head-title">Сортировка</span></th>
+                <th scope="col"><span class="main-grid-head-title">Активность</span></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($categories as $category)
+                <tr class="js-product-item" wire:key="product-{{$category['id']}}">
+                    <td>
+                        <div class="form-check">
+                            <input wire:model.defer="categories.{{$category['id']}}.is_checked" @if($editMode) disabled @endif class="form-check-input position-static" type="checkbox">
+                        </div>
+                    </td>
+                    <td>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary" type="button" id="actions-dropdown-{{$category['id']}}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fa fa-bars"></i></button>
+                            <div class="dropdown-menu" aria-labelledby="actions-dropdown-{{$category['id']}}">
+                                <a class="dropdown-item" href="{{route("admin.products.index", ['category_id' => $category['id']])}}">Товары</a>
+                                <a class="dropdown-item" href="{{route("admin.categories.edit", $category['id'])}}">Изменить</a>
+                                <button type="button" class="dropdown-item btn btn-link" wire:click="toggleActive({{$category['id']}})">
+                                    @if($category['is_active'])
+                                        Деактивировать
+                                    @else
+                                        Активировать
+                                    @endif
+                                </button>
+                                <button type="button" class="dropdown-item btn btn-link" onclick="@this.handleDelete({{$category['id']}}).then(res => console.log(res))">Удалить</button>
+                            </div>
+                        </div>
+                    </td>
+                    <td><span class="main-grid-cell-content">{{$category['id']}}</span></td>
+                    <td>
+                        @if($editMode && $category['is_checked'])
+                            @include('admin.livewire.includes.form-control-input', ['field' => "categories.{$category['id']}.name"])
+                        @else
+                            <span class="main-grid-cell-content"><a href="#" wire:click.prevent="navigate({{$category['id']}})"><i class="fa fa-folder" aria-hidden="true"></i> {!! $category['name'] !!}</a></span>
+                        @endif
+                    </td>
+                    <td @if($editMode && $category['is_checked']) style="width: 200px;" @endif>
+                        @if($editMode && $category['is_checked'])
+                            @include('admin.livewire.includes.form-control-input', ['field' => "categories.{$category['id']}.ordering"])
+                        @else
+                            <span class="main-grid-cell-content">{{$category['ordering']}}</span>
+                        @endif
+                    </td>
+                    <td>
+                        @if($editMode && $category['is_checked'])
+                            @include('admin.livewire.includes.form-check', ['field' => "categories.{$category['id']}.is_active"])
+                        @else
+                            <span class="main-grid-cell-content">{{$category['is_active_name']}}</span>
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    <div class="row pb-5">
+        @if(!$editMode)
+            <div class="col-sm-3" wire:key="edit-btn">
+                <button wire:click.prevent="$set('editMode', true)" type="button" class="btn btn-light"><i class="fa fa-edit"></i> Редактировать</button>
+            </div>
+            <div class="col-sm-3" wire:key="delete-btn">
+                <button onclick="@this.deleteSelected().then(res => console.log(res))" type="button" class="btn btn-light"><i class="fa fa-times"></i> Удалить</button>
+            </div>
+        @else
+            <div class="col-sm-3" wire:key="save-btn">
+                <button wire:click.prevent="saveSelected" type="button" class="btn btn-info">Сохранить</button>
+            </div>
+            <div class="col-sm-3" wire:key="cancel-btn">
+                <button wire:click.prevent="cancelEdit" type="button" class="btn btn-warning">Отменить</button>
+            </div>
+        @endif
+    </div>
+    <div class="row pb-5">
+        @foreach($errors->all() as $error)
+            <div wire:key="{{$error}}" class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{$error}}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endforeach
+    </div>
+</div>
---
+++ src/App/Http/Livewire/Admin/ShowCategory.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use App\Rules\CategoryDeactivatable;
+use Domain\Products\Actions\DeleteCategoryAction;
+use Domain\Products\Actions\GetCategoryAndSubtreeIdsAction;
+use Domain\Products\DTOs\CategoryProductItemAdminDTO;
+use Domain\Products\Models\Category;
+use Domain\Products\Models\Product\Product;
+use Illuminate\Validation\Rules\Exists;
+use Illuminate\Validation\Rules\Unique;
+use Livewire\Component;
+
+class ShowCategory extends Component
+{
+    use HasTabs;
+    use HasSeo;
+    use HasCategories;
+    use HasGenerateSlug;
+
+    protected const DEFAULT_TAB = 'elements';
+
+    /**
+     * @var \Domain\Products\Models\Category
+     */
+    public Category $item;
+
+    /**
+     * @var array[] @see {@link \Domain\Products\DTOs\CategoryProductItemAdminDTO}
+     */
+    public array $products = [];
+
+    /**
+     * @var bool
+     */
+    public bool $generateSlugSyncMode = false;
+
+    /**
+     * @var string[]
+     */
+    public array $tabs = [
+        'elements' => 'Элемент',
+        'seo' => 'SEO',
+        'product-list' => 'Товары',
+    ];
+
+    /**
+     * @return string[]|array[]
+     */
+    protected function rules(): array
+    {
+        $notIn = [];
+        if ($this->item->id) {
+            $notIn = GetCategoryAndSubtreeIdsAction::cached()->execute($this->item->id);
+        }
+
+        return array_merge(
+            $this->getSeoRules(),
+            [
+                'item.name' => 'required|string|max:199',
+                'item.slug' => [
+                    'required',
+                    'string',
+                    'max:199',
+                    (new Unique(Category::TABLE, 'slug'))
+                        ->when(!!$this->item->id, function(Unique $unique) {
+                            return $unique->ignore($this->item->id);
+                        }),
+                ],
+                'item.ordering' => 'integer|nullable',
+                'item.is_active' => [
+                    'nullable',
+                    'boolean',
+                    ($this->item->id ? new CategoryDeactivatable() : 'nullable'),
+                ],
+                'item.parent_id' => [
+                    'nullable',
+                    (new Exists(Category::TABLE, 'id'))
+                        ->when(!empty($notIn), function(Exists $exists) use($notIn) {
+                            return $exists->whereNotIn('id', $notIn);
+                        }),
+                ],
+                'item.description' => 'nullable|max:65000',
+            ]
+        );
+    }
+
+    protected $messages = [
+        'item.parent_id.exists' => 'У родительской категорией не может быть сама категория или её подкатегории.',
+    ];
+
+    public function mount()
+    {
+        $this->initTabs();
+        $this->initSeo();
+
+        $exclude = [];
+        if ($this->item->id) {
+            $exclude = GetCategoryAndSubtreeIdsAction::cached()->execute($this->item->id);
+        }
+        $this->initCategories($exclude);
+        $this->initGenerateSlug();
+
+        $this->products = $this->item
+            ->products()
+            ->select(['id', 'name', 'is_active', 'category_id'])
+            ->get()
+            ->map(fn(Product $product) => CategoryProductItemAdminDTO::fromModel($product)->toArray())
+            ->all();
+    }
+
+    public function render()
+    {
+        return view('admin.livewire.show-category.show-category');
+    }
+
+    public function save()
+    {
+        $this->validate();
+
+        $shouldRedirect = false;
+        if (!$this->item->id) {
+            $shouldRedirect = true;
+        }
+        $this->saveItem();
+        $this->saveSeo();
+
+        if ($shouldRedirect) {
+            return redirect()->route('admin.categories.edit', $this->item->id);
+        }
+    }
+
+    public function deleteItem()
+    {
+        if ($this->item->has_active_products) {
+            $this->addError('delete', sprintf('Категория с id %s не может быть удалена, пока у этой категории и или у её подкатегорий есть активные продукты.', $this->item->id));
+            return false;
+        }
+        $parentId = $this->item->parent_id;
+        DeleteCategoryAction::cached()->execute($this->item);
+        return redirect()->route('admin.categories.index', ['category_id' => $parentId]);
+    }
+
+    public function clearValidationErrors()
+    {
+        $this->clearValidation();
+    }
+
+    public function updatedItemName()
+    {
+        $this->handleGenerateSlug();
+    }
+
+    protected function saveItem()
+    {
+        $this->item->save();
+    }
+}
