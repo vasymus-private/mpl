0d1924b097051b48aee780159ce6e282c5f6c671
sergey <sergey.martishko@yandex.ru>
2021-06-17 23:45:38 +0300

    add markup for product

--- resources/views/admin/livewire/show-product/tab-other.blade.php
+++ resources/views/admin/livewire/show-product/tab-other.blade.php
@@ -13,7 +13,7 @@
                 <div class="other-page__item">
                     <div class="form-check form-check-inline">
                         <input wire:model="item.category_id" class="form-check-input" type="radio" name="main-category" value="{{$category['value']}}" />
-                        <input wire:model="relatedCategories" class="form-check-input" type="checkbox" value="{{$category['value']}}" id="related-category-{{$category['value']}}" />
+                        <input wire:model="relatedCategories" class="custom-checkbox" type="checkbox" value="{{$category['value']}}" id="related-category-{{$category['value']}}" />
                         <label for="related-category-{{$category['value']}}" class="form-check-label">{{$category['label']}}</label>
                     </div>
                 </div>
