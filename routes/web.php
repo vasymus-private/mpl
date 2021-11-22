6709a51f4260856ca7b0a03315e96a356f338831
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2021-11-22 16:26:51 +0200

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Constants;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -20,7 +21,7 @@ Route::get('---test-email-order-markup', [\App\Http\Controllers\TestController::
 Route::get("---test-email-password-reset", [\App\Http\Controllers\TestController::class, "testResetPasswordMarkup"])->middleware('auth');
 
 
-Route::middleware([\App\Constants::MIDDLEWARE_AUTHENTICATE_ALL])->group(function() {
+Route::middleware([Constants::MIDDLEWARE_AUTHENTICATE_ALL])->group(function() {
 
     /** @see \Laravel\Ui\AuthRouteMethods::auth() */
     Route
