fde6cf9261f3b46bd0d9a62e96b8ef8b4dcdefdf
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2022-01-30 18:44:01 +0200

    admin: sticky footer / header

---
+++ resources/js/admin/modules/footerSticky.js
@@ -0,0 +1,76 @@
+(() => {
+    let wrapperSelector = '.js-edit-footer-wrapper'
+    let footerSelector = '.js-edit-item-footer'
+    let fixedClass = 'edit-item-footer-fixed'
+    let pinButtonSelector = '.js-pin-btn'
+    let pinButtonPinnedClass = 'pin-btn-pinned'
+    let footerMarkerSelector = '.js-edit-item-footer-marker'
+
+    let $window = $(window)
+    let $wrappers = $(wrapperSelector)
+
+    $wrappers.each((i, el) => {
+        let $wrapper = $(el)
+        let $btn = $wrapper.find(pinButtonSelector)
+        let $footer = $wrapper.find(footerSelector)
+        let $maker = $wrapper.find(footerMarkerSelector)
+        let isPinned = true
+
+        setIsPinned(isPinned)
+        main()
+
+        $btn.on('click', () => {
+            toggleIsPinned()
+        })
+
+        $('[data-toggle="tab"]').on('shown.bs.tab', () => {
+            main()
+        })
+
+        $window.on('scroll', () => {
+            main()
+        })
+
+        function main() {
+            if (!getIsPinned($btn)) {
+                return true
+            }
+
+            let isVisible = getIsVisible()
+            if (isVisible) {
+                $footer.removeClass(fixedClass)
+                return true
+            }
+
+            $footer.addClass(fixedClass)
+        }
+
+        function getIsPinned() {
+            return isPinned
+        }
+
+        function toggleIsPinned() {
+            setIsPinned(!getIsPinned())
+        }
+
+        function setIsPinned(_isPinned) {
+            isPinned = _isPinned
+            if (!_isPinned) {
+                $btn.removeClass(pinButtonPinnedClass)
+                $footer.removeClass(fixedClass)
+                return
+            }
+            $btn.addClass(pinButtonPinnedClass)
+        }
+
+        function getIsVisible() {
+            let footerTop = $maker.offset().top
+            let scrollTop = $window.scrollTop()
+            let space = $window.height() - $footer.height()
+
+            let compare = scrollTop + space + 20
+
+            return footerTop < compare
+        }
+    })
+})()
--- resources/views/admin/livewire/show-order/show-order.blade.php
+++ resources/views/admin/livewire/show-order/show-order.blade.php
@@ -64,13 +64,16 @@
         </div>
     </div>
 
-    <ul class="nav nav-tabs item-tabs" role="tablist">
-        @foreach($this->tabs() as $tab => $label)
-            <li wire:key="{{ $tab }}" class="nav-item" role="presentation">
-                <a wire:click="selectTab('{{$tab}}')" wire:ignore.self class="nav-link @if($tab === $activeTab) active @endif" data-toggle="tab" id="{{$tab}}-tab" href="#{{$tab}}" role="tab" aria-controls="{{$tab}}" aria-selected="{{$tab === $activeTab ? 'true' : 'false'}}">{{$label}}</a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="js-nav-tabs-wrapper">
+        <div class="js-nav-tabs-marker"></div>
+        <ul class="nav nav-tabs js-nav-tabs item-tabs" role="tablist">
+            @foreach($this->tabs() as $tab => $label)
+                <li wire:key="{{ $tab }}" class="nav-item" role="presentation">
+                    <a wire:click="selectTab('{{$tab}}')" wire:ignore.self class="nav-link @if($tab === $activeTab) active @endif" data-toggle="tab" id="{{$tab}}-tab" href="#{{$tab}}" role="tab" aria-controls="{{$tab}}" aria-selected="{{$tab === $activeTab ? 'true' : 'false'}}">{{$label}}</a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 
     <form wire:submit.prevent="handleSave" id="show-order-form" class="position-relative">
         @include('admin.livewire.includes.loading', ['target' => 'handleSave'])
@@ -89,10 +92,13 @@
             @endforeach
         </div>
 
-        <div class="edit-item-footer">
-            <button form="show-order-form" type="submit" class="btn btn-primary mb-2 btn__save mr-2">Сохранить</button>
-
-            <a href="{{route(\App\Constants::ROUTE_ADMIN_ORDERS_INDEX)}}" type="button" class="btn btn-info mb-2 btn__default">Отменить</a>
+        <div wire:ignore class="js-edit-footer-wrapper">
+            <div class="js-edit-item-footer-marker"></div>
+            <div class="edit-item-footer js-edit-item-footer">
+                <button form="show-order-form" type="submit" class="btn btn-primary mb-2 btn__save mr-2">Сохранить</button>
+                <a href="{{route(\App\Constants::ROUTE_ADMIN_ORDERS_INDEX)}}" type="button" class="btn btn-info mb-2 btn__default">Отменить</a>
+                <button type="button" class="btn btn-info js-pin-btn pin-btn"></button>
+            </div>
         </div>
     </form>
 
