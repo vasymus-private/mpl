e6f059e2ce72c89b6371b6df4f86c2b6e9aeb955
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2022-05-23 18:36:58 +0100

    [admin]: edit button for lists finish [phpstan fixes]

--- src/App/Http/Livewire/Admin/HasSelectAll.php
+++ src/App/Http/Livewire/Admin/HasSelectAll.php
@@ -8,30 +8,30 @@ trait HasSelectAll
 
     public function updatedSelectAll(bool $isChecked)
     {
-        if (isset($this->items)) {
-            $this->changeItemsSelectAll($isChecked);
-        }
+        $this->changeItemsSelectAll($isChecked);
 
-        if (isset($this->variations)) {
-            $this->changeVariationsSelectAll($isChecked);
-        }
+        $this->changeVariationsSelectAll($isChecked);
     }
 
     protected function changeItemsSelectAll(bool $isChecked)
     {
-        $this->items = collect($this->items)->map(function (array $item) use ($isChecked) {
-            $item['is_checked'] = $isChecked;
+        if (isset($this->items)) {
+            $this->items = collect($this->items)->map(function (array $item) use ($isChecked) {
+                $item['is_checked'] = $isChecked;
 
-            return $item;
-        })->all();
+                return $item;
+            })->all();
+        }
     }
 
     protected function changeVariationsSelectAll(bool $isChecked)
     {
-        $this->variations = collect($this->variations)->map(function (array $item) use ($isChecked) {
-            $item['is_checked'] = $isChecked;
+        if (isset($this->variations)) {
+            $this->variations = collect($this->variations)->map(function (array $item) use ($isChecked) {
+                $item['is_checked'] = $isChecked;
 
-            return $item;
-        })->all();
+                return $item;
+            })->all();
+        }
     }
 }
