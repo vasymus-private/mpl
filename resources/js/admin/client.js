00d2134f12024b7d1b48a7fcb4c720453bd664ae
vasymus <vasymus@users.noreply.github.com>
2022-05-29 23:33:45 +0000

    PHP CS changes

--- resources/js/admin/client.js
+++ resources/js/admin/client.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue'
+import Vue from "vue"
+import { createInertiaApp } from "@inertiajs/inertia-vue"
 
 createInertiaApp({
-    resolve: name => require(`./Pages/${name}`),
+    resolve: (name) => require(`./Pages/${name}`),
     setup({ el, App, props, plugin }) {
         Vue.use(plugin)
 
         new Vue({
-            render: h => h(App, props),
+            render: (h) => h(App, props),
         }).$mount(el)
     },
 })
--- src/App/Http/Middleware/HandleInertiaRequests.php
+++ src/App/Http/Middleware/HandleInertiaRequests.php
@@ -40,6 +40,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'auth' => function () {
                 $userOrAdmin = H::userOrAdmin();
+
                 return [
                     'user' => $userOrAdmin
                         ? array_merge($userOrAdmin->only([
