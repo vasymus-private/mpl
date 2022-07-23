d701af298e6385ce320887c2d4c643eeeac94331
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2022-07-23 13:37:24 +0100

    admin: inertia: product create / edit vee-validate + pinia; finish DescriptionTab.vue

--- resources/js/admin/inertia/components/products/tabs/DescriptionTab.vue
+++ resources/js/admin/inertia/components/products/tabs/DescriptionTab.vue
@@ -7,13 +7,22 @@ import {getRouteUrl, routeNames} from "@/admin/inertia/modules/routes"
 
 
 const productsStore = useProductsStore()
-const {setValue, value} = useField('preview')
+const {setValue : setPreviewValue, value : previewValue} = useField('preview')
+const {setValue : setDescriptionValue, value : descriptionValue} = useField('description')
 const preview = computed({
     get() {
-        return value.value || null
+        return previewValue.value || null
     },
     set(nv) {
-        return setValue(nv)
+        return setPreviewValue(nv)
+    }
+})
+const description = computed({
+    get() {
+        return descriptionValue.value || null
+    },
+    set(nv) {
+        return setDescriptionValue(nv)
     }
 })
 const uploadUrl = computed(() => {
@@ -69,6 +78,57 @@ const uploadUrl = computed(() => {
             <div class="tab-pane fade show active" id="preview-editor" role="tabpanel" aria-labelledby="preview-editor-tab">
                 <AppHtmlEditor
                     v-model="preview"
+                    :upload-url="uploadUrl"
+                />
+            </div>
+        </div>
+
+        <div class="h2">Детальное описание</div>
+
+        <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item" role="presentation">
+                <button
+                    class="nav-link"
+                    id="description-html-tab"
+                    data-bs-toggle="tab"
+                    data-bs-target="#description-html"
+                    type="button"
+                    role="tab"
+                    aria-controls="description-html"
+                    aria-selected="false"
+                >HTML</button>
+            </li>
+            <li class="nav-item" role="presentation">
+                <button
+                    class="nav-link active"
+                    id="description-editor-tab"
+                    data-bs-toggle="tab"
+                    data-bs-target="#description-editor"
+                    type="button"
+                    role="tab"
+                    aria-controls="description-editor"
+                    aria-selected="true"
+                >Визуальный редактор</button>
+            </li>
+        </ul>
+
+        <div class="tab-content" id="nav-description-tab-content">
+            <div class="tab-pane fade" style="height: 600px" id="description-html" role="tabpanel" aria-labelledby="description-html-tab">
+                <Field
+                    v-slot="{field, meta}"
+                    name="description"
+                >
+                    <textarea
+                        v-bind="field"
+                        :class="['form-control', 'h-100', !meta.valid ? 'is-invalid' : '']"
+                        id="description"
+                        rows="3"
+                    />
+                </Field>
+            </div>
+            <div class="tab-pane fade show active" id="description-editor" role="tabpanel" aria-labelledby="description-editor-tab">
+                <AppHtmlEditor
+                    v-model="description"
                     :upload-url="uploadUrl"
                 />
             </div>
