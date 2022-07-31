ada68b4e212c86aaf1c4686197beddbac753a05b
vasymus <vasymus@users.noreply.github.com>
2022-07-31 11:51:10 +0000

    PHP CS changes

--- resources/js/admin/inertia/modules/forms/createEditProduct/interfaces.ts
+++ resources/js/admin/inertia/modules/forms/createEditProduct/interfaces.ts
@@ -1,4 +1,4 @@
-import {Variation} from "@/admin/inertia/modules/products/Product";
+import { Variation } from "@/admin/inertia/modules/products/Product"
 
 export interface VariationForm extends Partial<Variation> {
     is_checked?: boolean
