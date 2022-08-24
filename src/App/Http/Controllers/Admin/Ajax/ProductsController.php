82bf00d08f56c9c6e27e798c3291b9c92382ab72
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2022-08-24 20:26:42 +0100

    admin: inertia: create / edit product: backend validation WIP

--- src/App/Http/Controllers/Admin/Ajax/ProductsController.php
+++ src/App/Http/Controllers/Admin/Ajax/ProductsController.php
@@ -6,21 +6,20 @@ use App\Http\Controllers\Admin\BaseAdminController;
 use App\Http\Requests\Admin\Ajax\CreateProductRequest;
 use App\Http\Requests\Admin\Ajax\UpdateProductRequest;
 use App\Http\Resources\Admin\ProductResource;
+use Domain\Products\Actions\CreateOrUpdateProductAction;
 use Domain\Products\Models\Product\Product;
 
 class ProductsController extends BaseAdminController
 {
     /**
      * @param \App\Http\Requests\Admin\Ajax\CreateProductRequest $request
+     * @param \Domain\Products\Actions\CreateOrUpdateProductAction
      *
      * @return \Illuminate\Contracts\Support\Responsable
      */
-    public function store(CreateProductRequest $request)
+    public function store(CreateProductRequest $request, CreateOrUpdateProductAction $createOrUpdateProductAction)
     {
-        dd($request->prepare());
-
-        /** @var \Domain\Products\Models\Product\Product $product */
-        $product = $request->admin_product;
+        $product = $createOrUpdateProductAction->execute($request->prepare());
 
         return new ProductResource(
             Product::query()->findOrFail($product->id)
@@ -29,126 +28,16 @@ class ProductsController extends BaseAdminController
 
     /**
      * @param \App\Http\Requests\Admin\Ajax\UpdateProductRequest $request
+     * @param \Domain\Products\Actions\CreateOrUpdateProductAction
      *
      * @return \Illuminate\Contracts\Support\Responsable
      */
-    public function update(UpdateProductRequest $request)
+    public function update(UpdateProductRequest $request, CreateOrUpdateProductAction $createOrUpdateProductAction)
     {
-        dd($request->prepare());
+        /** @var \Domain\Products\Models\Product\Product $target */
+        $target = $request->admin_product;
 
-
-        /** @var \Domain\Products\Models\Product\Product $product */
-        $product = $request->admin_product;
-
-        if ($request->has('name')) {
-            $product->name = $request->name;
-        }
-
-        if ($request->has('slug')) {
-            $product->slug = $request->slug;
-        }
-
-        if ($request->has('ordering')) {
-            $product->ordering = $request->ordering;
-        }
-
-        if ($request->has('is_active')) {
-            $product->is_active = $request->is_active;
-        }
-
-        if ($request->has('unit')) {
-            $product->unit = $request->unit;
-        }
-
-        if ($request->has('price_purchase')) {
-            $product->price_purchase = $request->price_purchase;
-        }
-
-        if ($request->has('price_purchase_currency_id')) {
-            $product->price_purchase_currency_id = $request->price_purchase_currency_id;
-        }
-
-        if ($request->has('price_retail')) {
-            $product->price_retail = $request->price_retail;
-        }
-
-        if ($request->has('price_retail_currency_id')) {
-            $product->price_retail_currency_id = $request->price_retail_currency_id;
-        }
-
-        if ($request->has('admin_comment')) {
-            $product->admin_comment = $request->admin_comment;
-        }
-
-        if ($request->has('availability_status_id')) {
-            $product->availability_status_id = $request->availability_status_id;
-        }
-
-        if ($request->has('parent_id')) {
-            $product->parent_id = $request->parent_id;
-        }
-
-        if ($request->has('brand_id')) {
-            $product->brand_id = $request->brand_id;
-        }
-
-        if ($request->has('category_id')) {
-            $product->category_id = $request->category_id;
-        }
-
-        if ($request->has('is_with_variations')) {
-            $product->is_with_variations = $request->is_with_variations;
-        }
-
-        if ($request->has('coefficient')) {
-            $product->coefficient = $request->coefficient;
-        }
-
-        if ($request->has('coefficient_description')) {
-            $product->coefficient_description = $request->coefficient_description;
-        }
-
-        if ($request->has('coefficient_description_show')) {
-            $product->coefficient_description_show = $request->coefficient_description_show;
-        }
-
-        if ($request->has('coefficient_variation_description')) {
-            $product->coefficient_variation_description = $request->coefficient_variation_description;
-        }
-
-        if ($request->has('price_name')) {
-            $product->price_name = $request->price_name;
-        }
-
-        if ($request->has('preview')) {
-            $product->preview = $request->preview;
-        }
-
-        if ($request->has('description')) {
-            $product->description = $request->description;
-        }
-
-        if ($request->has('accessory_name')) {
-            $product->accessory_name = $request->accessory_name;
-        }
-
-        if ($request->has('similar_name')) {
-            $product->similar_name = $request->similar_name;
-        }
-
-        if ($request->has('related_name')) {
-            $product->related_name = $request->related_name;
-        }
-
-        if ($request->has('work_name')) {
-            $product->work_name = $request->work_name;
-        }
-
-        if ($request->has('instruments_name')) {
-            $product->instruments_name = $request->instruments_name;
-        }
-
-        $product->save();
+        $product = $createOrUpdateProductAction->execute($request->prepare(), $target);
 
         return new ProductResource(
             Product::query()->findOrFail($product->id)
---
+++ src/Domain/Products/Actions/CreateOrUpdateProductAction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Domain\Products\Actions;
+
+use Domain\Common\Actions\BaseAction;
+use Domain\Products\DTOs\Admin\Inertia\CreateEditProduct\ProductDTO;
+use Domain\Products\Models\Product\Product;
+
+class CreateOrUpdateProductAction extends BaseAction
+{
+    /**
+     * @param \Domain\Products\DTOs\Admin\Inertia\CreateEditProduct\ProductDTO $productDTO
+     * @param \Domain\Products\Models\Product\Product|null $target
+     *
+     * @return \Domain\Products\Models\Product\Product
+     */
+    public function execute(ProductDTO $productDTO, Product $target = null): Product
+    {
+
+    }
+}
