1febc578ab915a7cf983b932108db9bf7ea69510
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2022-08-24 15:08:24 +0100

    admin: inertia: create / edit product: backend validation WIP

--- src/App/Http/Requests/Admin/Ajax/HasCreateUpdateProductRequest.php
+++ src/App/Http/Requests/Admin/Ajax/HasCreateUpdateProductRequest.php
@@ -7,37 +7,52 @@ use Domain\Products\DTOs\Admin\Inertia\CreateEditProduct\ProductDTO;
 use Domain\Products\Models\AvailabilityStatus;
 use Domain\Products\Models\Brand;
 use Domain\Products\Models\Category;
+use Domain\Products\Models\CharCategory;
+use Domain\Products\Models\CharType;
+use Domain\Products\Models\Product\Product;
 use Support\H;
 
 /**
- * @property string|null $name
- * @property string|null $slug
- * @property int|null $ordering
- * @property bool|null $is_active
- * @property string|null $unit
- * @property float|null $price_purchase
- * @property int|null $price_purchase_currency_id
- * @property float|null $price_retail
- * @property int|null $price_retail_currency_id
- * @property string|null $admin_comment
- * @property int|null $availability_status_id
- * @property int|null $parent_id
- * @property int|null $brand_id
- * @property int|null $category_id
- * @property bool|null $is_with_variations
- * @property float|null $coefficient
- * @property string|null $coefficient_description
- * @property bool|null $coefficient_description_show
- * @property string|null $coefficient_variation_description
- * @property string|null $price_name
- * @property string|null $preview
- * @property string|null $description
- * @property string|null $accessory_name
- * @property string|null $similar_name
- * @property string|null $related_name
- * @property string|null $work_name
- * @property string|null $instruments_name
- * @property array|null $seo
+ * @property-read string|null $name
+ * @property-read string|null $slug
+ * @property-read int|null $ordering
+ * @property-read bool|null $is_active
+ * @property-read string|null $unit
+ * @property-read float|null $price_purchase
+ * @property-read int|null $price_purchase_currency_id
+ * @property-read float|null $price_retail
+ * @property-read int|null $price_retail_currency_id
+ * @property-read string|null $admin_comment
+ * @property-read int|null $availability_status_id
+ * @property-read int|null $brand_id
+ * @property-read int|null $category_id
+ * @property-read bool|null $is_with_variations
+ * @property-read float|null $coefficient
+ * @property-read string|null $coefficient_description
+ * @property-read bool|null $coefficient_description_show
+ * @property-read string|null $coefficient_variation_description
+ * @property-read string|null $price_name
+ * @property-read string|null $preview
+ * @property-read string|null $description
+ * @property-read string|null $accessory_name
+ * @property-read string|null $similar_name
+ * @property-read string|null $related_name
+ * @property-read string|null $work_name
+ * @property-read string|null $instruments_name
+ * @property-read int[]|null $relatedCategoriesIds
+ * @property-read array|null $seo
+ * @property-read array|null $infoPrices
+ * @property-read array|null $instructions
+ * @property-read array|null $mainImage
+ * @property-read array|null $additionalImages
+ * @property-read array|null $charCategories
+ * @property-read array|null $chars
+ * @property-read int[]|null $accessories
+ * @property-read int[]|null $similar
+ * @property-read int[]|null $related
+ * @property-read int[]|null $works
+ * @property-read int[]|null $instruments
+ * @property-read array|null $variations
  */
 trait HasCreateUpdateProductRequest
 {
@@ -58,6 +73,14 @@ trait HasCreateUpdateProductRequest
      */
     public function rules()
     {
+        $currenciesIdsStr = Currency::cachedAll()->implode('id', ',');
+        $availabilityStatusIdsStr = AvailabilityStatus::cachedAll()->implode('id', ',');
+        $brandIdsStr = Brand::cachedAll()->implode('id', ',');
+        $categoriesIdsStr = Category::cachedAll()->implode('id', ',');
+        $charTypeIds = CharType::cachedAll()->implode('id', ',');
+        $charCategoryIds = CharCategory::cachedAll()->implode('id', ',');
+        $productIds = Product::cachedAll()->implode('id', ',');
+
         return [
             'name' => 'required|string|max:250',
             'slug' => 'required|string|max:250',
@@ -65,13 +88,13 @@ trait HasCreateUpdateProductRequest
             'is_active' => 'nullable|boolean',
             'unit' => 'nullable|string|max:250',
             'price_purchase' => 'nullable|numeric',
-            'price_purchase_currency_id' => sprintf('nullable|integer|in:%s', Currency::cachedAll()->implode('id', ',')),
+            'price_purchase_currency_id' => sprintf('nullable|integer|in:%s', $currenciesIdsStr),
             'price_retail' => 'nullable|numeric',
-            'price_retail_currency_id' => sprintf('nullable|integer|in:%s', Currency::cachedAll()->implode('id', ',')),
+            'price_retail_currency_id' => sprintf('nullable|integer|in:%s', $currenciesIdsStr),
             'admin_comment' => 'nullable|string|max:250',
-            'availability_status_id' => sprintf('nullable|integer|in:%s', AvailabilityStatus::cachedAll()->implode('id', ',')),
-            'brand_id' => sprintf('nullable|integer|in:%s', Brand::cachedAll()->implode('id', ',')),
-            'category_id' => sprintf('nullable|integer|in:%s', Category::cachedAll()->implode('id', ',')),
+            'availability_status_id' => sprintf('nullable|integer|in:%s', $availabilityStatusIdsStr),
+            'brand_id' => sprintf('nullable|integer|in:%s', $brandIdsStr),
+            'category_id' => sprintf('nullable|integer|in:%s', $categoriesIdsStr),
             'is_with_variations' => 'nullable|boolean',
             'coefficient' => 'nullable|numeric',
             'coefficient_description' => 'nullable|string|max:250',
@@ -89,7 +112,7 @@ trait HasCreateUpdateProductRequest
             'instruments_name' => 'nullable|string|max:250',
 
             'relatedCategoriesIds' => 'nullable|array',
-            'relatedCategoriesIds.*' => sprintf('nullable|integer|in:%s', Category::cachedAll()->implode('id', ',')),
+            'relatedCategoriesIds.*' => sprintf('nullable|integer|in:%s', $categoriesIdsStr),
 
             'seo' => 'nullable|array',
             'seo.title' => 'nullable|string|max:250',
@@ -124,6 +147,69 @@ trait HasCreateUpdateProductRequest
             'additionalImages.*.name' => 'nullable|string|max:250',
             'additionalImages.*.file_name' => 'nullable|string|max:250',
             'additionalImages.*.order_column' => 'nullable|integer',
+            'additionalImages.*.file' => sprintf('nullable|file|max:%s', H::validatorMb(95)),
+
+            'charCategories' => 'nullable|array',
+            'charCategories.*.id' => 'nullable|integer',
+            'charCategories.*.uuid' => 'string',
+            'charCategories.*.name' => 'nullable|string|max:250',
+            'charCategories.*.ordering' => 'nullable|integer',
+
+            'chars' => 'nullable|array',
+            'chars.*.id' => 'nullable|integer',
+            'chars.*.name' => 'nullable|string|max:250',
+            'chars.*.value' => 'nullable|string|max:250',
+            'chars.*.type_id' => sprintf('nullable|integer|in:%s', $charTypeIds),
+            'chars.*.ordering' => 'nullable|integer',
+            'chars.*.category_id' => sprintf('nullable|integer|in:%s', $charCategoryIds),
+            'chars.*.category_uuid' => 'nullable|string',
+
+            'accessories' => 'nullable|array',
+            'accessories.*' => sprintf('nullable|integer|in:%s', $productIds),
+
+            'similar' => 'nullable|array',
+            'similar.*' => sprintf('nullable|integer|in:%s', $productIds),
+
+            'related' => 'nullable|array',
+            'related.*' => sprintf('nullable|integer|in:%s', $productIds),
+
+            'works' => 'nullable|array',
+            'works.*' => sprintf('nullable|integer|in:%s', $productIds),
+
+            'instruments' => 'nullable|array',
+            'instruments.*' => sprintf('nullable|integer|in:%s', $productIds),
+
+            'variations' => 'nullable|array',
+            'variations.*.id' => 'nullable|integer',
+            'variations.*.uuid' => 'string',
+            'variations.*.name' => 'nullable|string|max:250',
+            'variations.*.is_active' => 'nullable|boolean',
+            'variations.*.ordering' => 'nullable|integer',
+            'variations.*.coefficient' => 'nullable|numeric',
+            'variations.*.coefficient_description' => 'nullable|string|max:250',
+            'variations.*.unit' => 'nullable|string|max:250',
+            'variations.*.availability_status_id' => sprintf('nullable|integer|in:%s', $availabilityStatusIdsStr),
+            'variations.*.price_purchase' => 'nullable|numeric',
+            'variations.*.price_purchase_currency_id' => sprintf('nullable|integer|in:%s', $currenciesIdsStr),
+            'variations.*.price_retail' => 'nullable|numeric',
+            'variations.*.price_retail_currency_id' => sprintf('nullable|integer|in:%s', $currenciesIdsStr),
+            'variations.*.preview' => 'nullable|string|max:65000',
+
+            'variations.*.mainImage' => 'nullable|array',
+            'variations.*.mainImage.id' => 'nullable|integer',
+            'variations.*.mainImage.uuid' => 'string',
+            'variations.*.mainImage.name' => 'nullable|string|max:250',
+            'variations.*.mainImage.file_name' => 'nullable|string|max:250',
+            'variations.*.mainImage.order_column' => 'nullable|integer',
+            'variations.*.mainImage.file' => sprintf('nullable|file|max:%s', H::validatorMb(95)),
+
+            'variations.*.additionalImages' => 'nullable|array',
+            'variations.*.additionalImages.*.id' => 'nullable|integer',
+            'variations.*.additionalImages.*.uuid' => 'string',
+            'variations.*.additionalImages.*.name' => 'nullable|string|max:250',
+            'variations.*.additionalImages.*.file_name' => 'nullable|string|max:250',
+            'variations.*.additionalImages.*.order_column' => 'nullable|integer',
+            'variations.*.additionalImages.*.file' => sprintf('nullable|file|max:%s', H::validatorMb(95)),
         ];
     }
 
