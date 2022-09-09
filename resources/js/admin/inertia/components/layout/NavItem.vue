d9366262fc08c0ac18ed7569955eef64f2a11277
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2022-09-09 23:39:04 +0100

    admin: inertia: fix nav item navigation

--- resources/js/admin/inertia/components/layout/NavItem.vue
+++ resources/js/admin/inertia/components/layout/NavItem.vue
@@ -1,5 +1,8 @@
 <script lang="ts" setup>
 import {Link} from "@inertiajs/inertia-vue3"
+import {Inertia} from "@inertiajs/inertia"
+import {onMounted, onUnmounted, ref} from "vue"
+import {Collapse} from "bootstrap"
 
 
 const props = defineProps<{
@@ -11,9 +14,87 @@ const props = defineProps<{
     isActiveCollapse?: boolean
     isArrowSpace?: boolean
     navLinkClass?: string
+    navLinkTextHref?: string
 }>()
 
+const collapse = ref<HTMLElement>(null)
+const linkText = ref<HTMLElement>(null)
+const toShow = ref<boolean|null>(null)
+let notPreventCollapsing = ref<boolean>(false)
 
+const handleCollapse = (event) => {
+    if (!props.isCollapse) {
+        return
+    }
+
+    if (!props.navLinkTextHref || event.target !== linkText.value) {
+        handleToggleCollapse()
+        return
+    }
+
+    Inertia.visit(props.navLinkTextHref)
+}
+
+const handleToggleCollapse = () => {
+    if (!collapse.value) {
+        return
+    }
+
+    let instance = Collapse.getInstance(collapse.value)
+    if (!instance) {
+        return
+    }
+
+    if (toShow.value == null) {
+        return
+    }
+
+    notPreventCollapsing.value = true
+
+    if (toShow.value) {
+        instance.show()
+    } else {
+        instance.hide()
+    }
+}
+
+const getCollapseListener = (isShow: boolean) => event => {
+    event.stopPropagation()
+
+    if (notPreventCollapsing.value) {
+        return true
+    }
+    toShow.value = isShow
+    event.preventDefault()
+}
+
+const onCollapseEnd = () => {
+    notPreventCollapsing.value = false
+}
+
+onMounted(() => {
+    if (!collapse.value || !props.navLinkTextHref) {
+        return
+    }
+
+    collapse.value.addEventListener('show.bs.collapse', getCollapseListener(true))
+    collapse.value.addEventListener('shown.bs.collapse', onCollapseEnd)
+
+    collapse.value.addEventListener('hide.bs.collapse', getCollapseListener(false))
+    collapse.value.addEventListener('hidden.bs.collapse', onCollapseEnd)
+})
+
+onUnmounted(() => {
+    if (!collapse.value || !props.navLinkTextHref) {
+        return
+    }
+
+    collapse.value.removeEventListener('show.bs.collapse', getCollapseListener(true))
+    collapse.value.removeEventListener('shown.bs.collapse', onCollapseEnd)
+
+    collapse.value.removeEventListener('hide.bs.collapse', getCollapseListener(false))
+    collapse.value.removeEventListener('hidden.bs.collapse', onCollapseEnd)
+})
 </script>
 
 <template>
@@ -26,16 +107,18 @@ const props = defineProps<{
             :data-bs-target="props.isCollapse ? `#${props.idOrHref}` : null"
             :aria-expanded="!props.isCollapse ? null : (props.isCollapse && props.isActiveCollapse ? 'true' : 'false')"
             :aria-controls="!props.isCollapse ? null : props.idOrHref"
+            @click="handleCollapse"
         >
             <span v-if="props.isCollapse" class="adm-arrow-icon"></span>
             <span v-if="props.isArrowSpace" style="width: 20px;"></span>
             <span :class="props.iconClass"></span>
-            <span class="nav-link-text">{{ title }}</span>
+            <span ref="linkText" class="nav-link-text">{{ title }}</span>
         </component>
         <ul
             v-if="props.isCollapse"
             :id="props.idOrHref"
             :class="['nav', 'collapse', props.isActiveCollapse ? 'show' : '']"
+            ref="collapse"
         >
             <slot />
         </ul>
