365943e64664722d07f2ffb3200abb01b6c6c609
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2022-09-12 12:42:54 +0100

    admin: inertia: category create/edit: WIP some refactor

--- resources/js/admin/inertia/components/categories/createEdit/tabs/ElementsTab.vue
+++ resources/js/admin/inertia/components/categories/createEdit/tabs/ElementsTab.vue
@@ -3,22 +3,48 @@ import RowCheckbox from '@/admin/inertia/components/forms/vee-validate/RowCheckb
 import RowSelect from '@/admin/inertia/components/forms/vee-validate/RowSelect.vue'
 import RowInput from '@/admin/inertia/components/forms/vee-validate/RowInput.vue'
 import {useCreateEditCategoryFormStore} from "@/admin/inertia/modules/forms/createEditCategory"
-import DescriptionField from '@/admin/inertia/components/categories/createEdit/parts/DescriptionField.vue'
 import NameAndSlug from '@/admin/inertia/components/forms/parts/NameAndSlug.vue'
+import Description from "@/admin/inertia/components/forms/parts/Description.vue"
+import {useCategoriesStore} from "@/admin/inertia/modules/categories"
+import {computed} from "vue"
+import {getRouteUrl, routeNames} from "@/admin/inertia/modules/routes"
+
 
 const createEditCategoryFormStore = useCreateEditCategoryFormStore()
+const categoriesStore = useCategoriesStore()
+const uploadUrl = computed(() => {
+    return categoriesStore.category?.id
+        ? getRouteUrl(routeNames.ROUTE_ADMIN_AJAX_CATEGORY_IMAGE_UPLOAD, {admin_category: categoriesStore.category?.id})
+        : null
+})
 </script>
 
 <template>
     <div class="item-edit product-edit">
-        <RowCheckbox name="is_active" label="Активность" />
+        <RowCheckbox
+            name="is_active"
+            label="Активность"
+        />
 
-        <RowSelect name="parent_id" label="Родительский раздел" :options="createEditCategoryFormStore.parentCategoryOptions" />
+        <RowSelect
+            name="parent_id"
+            label="Родительский раздел"
+            :options="createEditCategoryFormStore.parentCategoryOptions"
+        />
 
         <NameAndSlug />
 
-        <RowInput name="ordering" label="Сортировка" type="number" />
+        <RowInput
+            name="ordering"
+            label="Сортировка"
+            type="number"
+        />
 
-        <DescriptionField />
+        <Description
+            id="description"
+            label="Детальное описание"
+            name="description"
+            :upload-url="uploadUrl"
+        />
     </div>
 </template>
--- resources/js/admin/inertia/components/products/createEdit/tabs/DescriptionTab.vue
+++ resources/js/admin/inertia/components/products/createEdit/tabs/DescriptionTab.vue
@@ -1,30 +1,12 @@
 <script lang="ts" setup>
-import {Field, useField} from "vee-validate"
 import {computed} from "vue"
-import AppHtmlEditor from '@/admin/inertia/components/forms/parts/AppHtmlEditor.vue'
 import {useProductsStore} from "@/admin/inertia/modules/products"
 import {getRouteUrl, routeNames} from "@/admin/inertia/modules/routes"
+import Description from "@/admin/inertia/components/forms/parts/Description.vue"
 
 
 const productsStore = useProductsStore()
-const {setValue : setPreviewValue, value : previewValue} = useField<string|null>('preview')
-const {setValue : setDescriptionValue, value : descriptionValue} = useField<string|null>('description')
-const preview = computed({
-    get() {
-        return previewValue.value || null
-    },
-    set(nv) {
-        return setPreviewValue(nv)
-    }
-})
-const description = computed({
-    get() {
-        return descriptionValue.value || null
-    },
-    set(nv) {
-        return setDescriptionValue(nv)
-    }
-})
+
 const uploadUrl = computed(() => {
     return productsStore.product?.id ? getRouteUrl(routeNames.ROUTE_ADMIN_AJAX_PRODUCT_IMAGE_UPLOAD, {admin_product: productsStore.product?.id}) : null
 })
@@ -32,106 +14,18 @@ const uploadUrl = computed(() => {
 
 <template>
     <div class="item-edit product-edit">
-        <div class="h2">Описание для анонса</div>
+        <Description
+            id="preview"
+            label="Описание для анонса"
+            name="preview"
+            :upload-url="uploadUrl"
+        />
 
-        <ul class="nav nav-tabs" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button
-                    class="nav-link"
-                    id="preview-html-tab"
-                    data-bs-toggle="tab"
-                    data-bs-target="#preview-html"
-                    type="button"
-                    role="tab"
-                    aria-controls="preview-html"
-                    aria-selected="false"
-                >HTML</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button
-                    class="nav-link active"
-                    id="preview-editor-tab"
-                    data-bs-toggle="tab"
-                    data-bs-target="#preview-editor"
-                    type="button"
-                    role="tab"
-                    aria-controls="preview-editor"
-                    aria-selected="true"
-                >Визуальный редактор</button>
-            </li>
-        </ul>
-
-        <div class="tab-content" id="nav-preview-tab-content">
-            <div class="tab-pane fade" style="height: 600px" id="preview-html" role="tabpanel" aria-labelledby="preview-html-tab">
-                <Field
-                    v-slot="{field, meta}"
-                    name="preview"
-                >
-                    <textarea
-                        v-bind="field"
-                        :class="['form-control', 'h-100', !meta.valid ? 'is-invalid' : '']"
-                        id="preview"
-                        rows="3"
-                    />
-                </Field>
-            </div>
-            <div class="tab-pane fade show active" id="preview-editor" role="tabpanel" aria-labelledby="preview-editor-tab">
-                <AppHtmlEditor
-                    v-model="preview"
-                    :upload-url="uploadUrl"
-                />
-            </div>
-        </div>
-
-        <div class="h2">Детальное описание</div>
-
-        <ul class="nav nav-tabs" role="tablist">
-            <li class="nav-item" role="presentation">
-                <button
-                    class="nav-link"
-                    id="description-html-tab"
-                    data-bs-toggle="tab"
-                    data-bs-target="#description-html"
-                    type="button"
-                    role="tab"
-                    aria-controls="description-html"
-                    aria-selected="false"
-                >HTML</button>
-            </li>
-            <li class="nav-item" role="presentation">
-                <button
-                    class="nav-link active"
-                    id="description-editor-tab"
-                    data-bs-toggle="tab"
-                    data-bs-target="#description-editor"
-                    type="button"
-                    role="tab"
-                    aria-controls="description-editor"
-                    aria-selected="true"
-                >Визуальный редактор</button>
-            </li>
-        </ul>
-
-        <div class="tab-content" id="nav-description-tab-content">
-            <div class="tab-pane fade" style="height: 600px" id="description-html" role="tabpanel" aria-labelledby="description-html-tab">
-                <Field
-                    v-slot="{field, meta}"
-                    name="description"
-                >
-                    <textarea
-                        v-bind="field"
-                        :class="['form-control', 'h-100', !meta.valid ? 'is-invalid' : '']"
-                        id="description"
-                        rows="3"
-                    />
-                </Field>
-            </div>
-            <div class="tab-pane fade show active" id="description-editor" role="tabpanel" aria-labelledby="description-editor-tab">
-                <AppHtmlEditor
-                    v-model="description"
-                    :upload-url="uploadUrl"
-                />
-            </div>
-        </div>
+        <Description
+            id="description"
+            label="Детальное описание"
+            name="description"
+            :upload-url="uploadUrl"
+        />
     </div>
 </template>
