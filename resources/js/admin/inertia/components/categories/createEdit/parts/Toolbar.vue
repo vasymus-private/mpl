f90387451e0b90d83efebd68255a24eb9953d63f
Vasyl Zhdankin <78029142+vz-spr@users.noreply.github.com>
2022-09-13 19:46:48 +0100

    admin: inertia: delete for index products and categories

--- resources/js/admin/inertia/components/categories/createEdit/parts/Toolbar.vue
+++ resources/js/admin/inertia/components/categories/createEdit/parts/Toolbar.vue
@@ -2,9 +2,15 @@
 import {Link} from "@inertiajs/inertia-vue3"
 import {routeNames} from "@/admin/inertia/modules/routes"
 import {useCategoriesStore} from "@/admin/inertia/modules/categories"
+import {useCreateEditCategoryFormStore} from "@/admin/inertia/modules/forms/createEditCategory";
 
 
 const categoriesStore = useCategoriesStore()
+const createEditCategoryFormStore = useCreateEditCategoryFormStore()
+
+const handleDelete = () => {
+
+}
 </script>
 
 <template>
@@ -17,9 +23,9 @@ const categoriesStore = useCategoriesStore()
                     <span class="detail-toolbar__btn-r"></span>
                 </Link>
             </div>
-            <div class="col-sm-5 d-flex align-items-center">
+            <div v-if="!categoriesStore.isCreatingCategoryRoute" class="col-sm-5 d-flex align-items-center">
                 <Link :href="route(routeNames.ROUTE_ADMIN_CATEGORIES_TEMP_CREATE)" class="btn btn-info mx-1">Добавить раздел</Link>
-                <button type="button" @click="categoriesStore.deleteCategory()" class="btn btn-danger mx-1">Удалить раздел</button>
+                <button type="button" @click="handleDelete" class="btn btn-danger mx-1">Удалить раздел</button>
             </div>
         </div>
     </div>
