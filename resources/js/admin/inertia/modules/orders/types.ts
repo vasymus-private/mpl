dc79e488312ab181dff1cddd33b389bae260fc04
vasymus <vasymus@users.noreply.github.com>
2022-09-16 21:38:37 +0000

    PHP CS changes

--- resources/js/admin/inertia/modules/orders/types.ts
+++ resources/js/admin/inertia/modules/orders/types.ts
@@ -24,15 +24,15 @@ export interface OrderItem {
 export interface Order {
     id: number
     user_id: number | null
-    user_name: string|null
-    user_email: string|null
-    user_phone: string|null
-    request_user_name: string|null
-    request_user_email: string|null
-    request_user_phone: string|null
+    user_name: string | null
+    user_email: string | null
+    user_phone: string | null
+    request_user_name: string | null
+    request_user_email: string | null
+    request_user_phone: string | null
     order_status_id: number | null
     cancelled: boolean
-    cancelled_description: string|null
+    cancelled_description: string | null
     payment_method_id: number | null
     comment_user: string | null
     initial_attachments: Array<Media>
@@ -41,29 +41,29 @@ export interface Order {
     admin_name: string | null
     admin_color: string | null
     importance_id: number | null
-    customer_bill_description: string|null
-    customer_bill_status_id: string|null
+    customer_bill_description: string | null
+    customer_bill_status_id: string | null
     customer_invoices: Array<Media>
-    provider_bill_status_id: number|null
-    provider_bill_description: string|null
+    provider_bill_status_id: number | null
+    provider_bill_description: string | null
     supplier_invoices: Array<Media>
     comment_admin: string | null
     products: Array<OrderProductItem>
     created_at: DateTime | null // parse from 'Y-m-d H:i:s'
     updated_at: DateTime | null // parse from 'Y-m-d H:i:s'
     is_busy_by_other_admin: boolean
-    busy_by_name: string|null
+    busy_by_name: string | null
 }
 
 export interface OrderItemProductItem {
     product: {
         id: number
         name: string
-        unit: string|null
+        unit: string | null
     }
     order_product: {
-        name: string|null
-        unit: string|null
+        name: string | null
+        unit: string | null
     }
 }
 
@@ -72,11 +72,11 @@ export interface OrderProductItem {
         id: number
         uuid: string
         name: string
-        unit: string|null
-        price_purchase: number|null
-        price_purchase_currency_id: number|null
-        price_retail: number|null
-        price_retail_currency_id: number|null
+        unit: string | null
+        price_purchase: number | null
+        price_purchase_currency_id: number | null
+        price_retail: number | null
+        price_retail_currency_id: number | null
     }
     order_product: {
         ordering: number
@@ -85,8 +85,8 @@ export interface OrderProductItem {
         price_purchase_currency_id: number
         price_retail: number
         price_retail_currency_id: number
-        name: string|null
-        unit: string|null
+        name: string | null
+        unit: string | null
         price_retail_rub: number
         price_retail_rub_origin: number
         price_retail_rub_was_updated: boolean
--- src/App/Http/Resources/Admin/OrderResource.php
+++ src/App/Http/Resources/Admin/OrderResource.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Resources\Admin;
 
-use Domain\Products\DTOs\Admin\OrderProductItemDTO;
-use Domain\Products\Models\Product\Product;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Carbon;
 
