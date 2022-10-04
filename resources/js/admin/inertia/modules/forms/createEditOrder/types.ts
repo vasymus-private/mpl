f6848b3fadc60deaa30cab248ef26820c60ad5dc
vasymus <vasymus@users.noreply.github.com>
2022-10-04 21:39:39 +0000

    PHP CS changes

--- resources/js/admin/inertia/modules/forms/createEditOrder/types.ts
+++ resources/js/admin/inertia/modules/forms/createEditOrder/types.ts
@@ -1,3 +1,24 @@
 import { Order } from "@/admin/inertia/modules/orders/types"
 
-export interface Values extends Partial<Omit<Order, 'user_id' | 'user_name' | 'user_email' | 'user_phone' | 'cancelled' | 'cancelled_description' | 'initial_attachments' | 'payment_method_attachments' | 'admin_name' | 'admin_color' | 'created_at' | 'dt_created_at' | 'updated_at' | 'dt_updated_at' | 'is_busy_by_other_admin' | 'busy_by_name'>> {}
+export interface Values
+    extends Partial<
+        Omit<
+            Order,
+            | "user_id"
+            | "user_name"
+            | "user_email"
+            | "user_phone"
+            | "cancelled"
+            | "cancelled_description"
+            | "initial_attachments"
+            | "payment_method_attachments"
+            | "admin_name"
+            | "admin_color"
+            | "created_at"
+            | "dt_created_at"
+            | "updated_at"
+            | "dt_updated_at"
+            | "is_busy_by_other_admin"
+            | "busy_by_name"
+        >
+    > {}
